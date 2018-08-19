40550d5da00fea053b5162bfbbfe9b4f615e500f
Victoria Meng <nights1ststar@protonmail.com>
2018-08-19 21:33:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function produceDrivingRange(range){
   return function(start, end){
-    const diff = end - start;
-    return diff >= 0 ? `Within range by ${diff}` : `${diff} blocks out of range`;
+    const distance = Math.abs(parseInt(end) - parseInt(start));
+    const range_diff = Math.abs(range - distance);
+    return diff >= range ? `Within range by ${diff}` : `${diff} blocks out of range`;
   };
 };
